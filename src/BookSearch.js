ce0ec3fd793335adce98011a0eaf712174896611
Charlie Kriewall <charlie@kriewall.com>
2018-03-13 09:38:24 -0700

    add addBookToShelf function to BookSearch component

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,28 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
-import Book from "./Book";
+import BookShelf from "./BookShelf";
 
 class BookSearch extends React.Component {
   state = {
     // TODO: replace with dynamic search.
     books: [],
-    query: ""
+    query: "",
+    shelf: [
+      {
+        id: "results",
+        name: "Search results"
+      }
+    ]
   };
 
   handleSearch(e) {
     // if search parameters are present, execute the API search
     if (e.target.value !== "") {
-      this.setState({query: e.target.value} )
+      this.setState({ query: e.target.value });
       BooksAPI.search(this.state.query).then(books => {
         this.setState({ books });
-        console.log(this.state.books)
+        console.log(this.state.books);
         // console.log(this.state.books);
       });
+    } else {
+      // if the search box is empty, empty the result set
+      this.setState({ books: [] });
     }
-    // if the search box is empty, empty the result set
-    else {this.setState({ books: []})}
   }
+
+  // function by @marcus https://udacity-react.slack.com/team/U9L3DB8CD
+  addBookToShelf = (bookToAdd, shelf) => {
+    this.setState(state => {
+      // return a new array that excludes the selected  book
+      const nextState = state.books.filter(book => book.id !== bookToAdd.id);
+      // append the selected book to the new array and include its target shelf prop
+      return {
+        books: [...nextState, { ...bookToAdd, shelf }]
+      };
+    });
+  };
 
   render() {
     return (
@@ -54,12 +73,15 @@ class BookSearch extends React.Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-
             {/* map search results to Book component */}
-            {this.state.books.map(book => (
-              <Book book={book} key={book.id} />
+            {this.state.shelf.map(shelf => (
+              <BookShelf
+                shelf={shelf}
+                key={shelf.id}
+                books={this.state.books}
+                addBookToShelf={this.addBookToShelf}
+              />
             ))}
-
           </ol>
         </div>
       </div>
